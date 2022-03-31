51475cbab9c5966bce5a7f9e94f4b6e5c769955c
Disha Shukla <dishashukla463@gmail.com>
2022-03-31 03:22:47 -0700

    Menus in Android

---
+++ app/src/main/java/com/example/learningmenus/MainActivity.java
@@ -0,0 +1,44 @@
+package com.example.learningmenus;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.ContextMenu;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+         super.onCreateOptionsMenu(menu);
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu_example, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        Toast.makeText(this, "Selected Item: " +item.getTitle(), Toast.LENGTH_SHORT).show();
+        switch (item.getItemId()) {
+            case R.id.mail:
+                // do something
+                return true;
+            case R.id.share:
+                // do something
+                return true;
+            default:
+                return super.onContextItemSelected(item);
+        }
+    }
+}
